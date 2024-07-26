ef01b9a57ca7b4d9f4b51254125a67fbf9b58b17
Boucetta Anfal Yousra <58882074+yous92@users.noreply.github.com>
2024-07-26 20:33:45 +0100

    Time: 38 ms (50.75%), Space: 16.7 MB (6.35%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,30 +3,55 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freq = Counter(s)
+#         freq = Counter(s)
     
-        # Create a max heap based on the frequency of characters
-        max_heap = [(-count, char) for char, count in freq.items()]
-        heapq.heapify(max_heap)
+#         # Create a max heap based on the frequency of characters
+#         max_heap = [(-count, char) for char, count in freq.items()]
+#         heapq.heapify(max_heap)
 
-        prev_count, prev_char = 0, ''
-        result = []
+#         prev_count, prev_char = 0, ''
+#         result = []
 
-        while max_heap:
-            count, char = heapq.heappop(max_heap)
-            # Append the current character to the result
-            result.append(char)
-            # If the previous character count is not zero, push it back to the heap
-            if prev_count < 0:
-                heapq.heappush(max_heap, (prev_count, prev_char))
+#         while max_heap:
+#             count, char = heapq.heappop(max_heap)
+#             # Append the current character to the result
+#             result.append(char)
+#             # If the previous character count is not zero, push it back to the heap
+#             if prev_count < 0:
+#                 heapq.heappush(max_heap, (prev_count, prev_char))
 
-            # Update the previous character and count (decrement count by 1)
-            prev_count, prev_char = count + 1, char
+#             # Update the previous character and count (decrement count by 1)
+#             prev_count, prev_char = count + 1, char
 
-        result = ''.join(result)
+#         result = ''.join(result)
 
-        # If the result length is equal to the input string length, return the result
-        if len(result) == len(s):
-            return result
-        else:
-            return ""
+#         # If the result length is equal to the input string length, return the result
+#         print(len(result))
+#         print(len(s))
+#         if len(result) == len(s):
+#             return result
+#         else:
+#             return ""
+            elt_freq=Counter(s)
+            max_heap=[(-value,key) for key ,value in elt_freq.items()]
+            heapq.heapify(max_heap)
+
+            ans=[]
+            freq_previous=0
+            letter_previous=''
+            rearangedstring=""
+            while max_heap:
+                freq,letter=heapq.heappop(max_heap)
+                ans.append(letter)
+                if freq_previous <0 :
+                    heapq.heappush(max_heap,(freq_previous,letter_previous))
+                freq_previous=freq+1
+                letter_previous=letter
+
+            for charr in ans:
+                rearangedstring+=charr
+            if len(rearangedstring)!=len(s):
+                return ""
+            else:
+                return rearangedstring
+        
