6c8d163b8d1aaadb177b7d6016014c42000ba098
Boucetta Anfal Yousra <58882074+yous92@users.noreply.github.com>
2024-08-20 19:35:20 +0100

    Time: 36 ms (55.54%), Space: 16.7 MB (7.18%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,17 +1,22 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # state -> index
-        # recursion pick no pick -> pick i and goto i + 2,, no pick go i + 1
-        # base case -> index > lenght of nums
-        memo = {}
-        def dp(i, last):
-            if i > last:
-                return 0
-            if (i, last) in memo:
-                return memo[(i, last)]
-            memo[i, last] = max(nums[i] + dp(i + 2, last), dp(i + 1, last))
-            return memo[(i, last)]
-        N = len(nums)
-        if N == 1:
+        
+        if len(nums) == 1:
             return nums[0]
-        return max(dp(0, N - 2), dp(1, N - 1))
+        memo = {}
+        def dp(start, end):
+            if start > end:
+                return 0
+            
+            if (start,end) in memo:
+                return memo[(start,end)]
+            
+            rob_this = nums[start] + dp(start + 2, end)
+            skip_this = dp(start + 1, end)
+
+            memo[(start,end)] = max(rob_this, skip_this)
+
+            return memo[(start,end)]
+
+  
+        return max(dp(0, len(nums) - 2), dp(1, len(nums) - 1))
