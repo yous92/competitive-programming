fefe6890a7da884b52edb98676a2a477358c43cf
Boucetta Anfal Yousra <58882074+yous92@users.noreply.github.com>
2024-09-27 09:07:30 +0100

    Time: 46 ms (51.19%), Space: 21.9 MB (7.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_s = ''.join(char.lower() for char in s if char.isalnum())
+        return filtered_s == filtered_s[::-1]
+       
