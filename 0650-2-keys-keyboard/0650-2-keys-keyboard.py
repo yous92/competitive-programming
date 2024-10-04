eaabfa7045be143e0d4f7dfd267773638effedc1
Boucetta Anfal Yousra <58882074+yous92@users.noreply.github.com>
2024-10-04 21:22:25 +0100

    Time: 34 ms (81.96%), Space: 16.5 MB (86.80%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+
+        operations = 0
+        factor = 2
+
+        while n > 1:
+            while n % factor == 0:
+                operations += factor
+                n //= factor
+            factor += 1
+
+        return operations
